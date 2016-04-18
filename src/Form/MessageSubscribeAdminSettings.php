d89bf6173a188bddce3e12797adb9ebe23b14a0c
Jonathan Hedstrom <jhedstrom@gmail.com>
2016-04-18 12:42:17 -0700

    Admin settings form + tests.

--- src/Form/MessageSubscribeAdminSettings.php
+++ src/Form/MessageSubscribeAdminSettings.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\message_subscribe\Form\MessageSubscribeAdminSettings.
- */
-
 namespace Drupal\message_subscribe\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
@@ -24,18 +19,15 @@ class MessageSubscribeAdminSettings extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
     $config = $this->config('message_subscribe.settings');
 
-    foreach (Element::children($form) as $variable) {
-      $config->set($variable, $form_state->getValue($form[$variable]['#parents']));
+    foreach (['use_queue', 'notify_own_actions', 'flag_prefix'] as $variable) {
+      $config->set($variable, $form_state->getValue($variable));
     }
+    $config->set('default_notifiers', array_values($form_state->getValue('default_notifiers')));
+
     $config->save();
-
-    if (method_exists($this, '_submitForm')) {
-      $this->_submitForm($form, $form_state);
-    }
-
-    parent::submitForm($form, $form_state);
   }
 
   /**
@@ -45,14 +37,23 @@ class MessageSubscribeAdminSettings extends ConfigFormBase {
     return ['message_subscribe.settings'];
   }
 
-  public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    /** @var \Drupal\Core\Plugin\DefaultPluginManager $message_notifiers */
+    $message_notifiers = \Drupal::service('plugin.message_notify.notifier.manager');
+    $options = array_map(function ($definition) {
+      return $definition['title'];
+    }, $message_notifiers->getDefinitions());
+
+    $config = $this->config('message_subscribe.settings');
+
     $form['default_notifiers'] = [
       '#type' => 'select',
       '#title' => t('Default message notifiers'),
       '#description' => t('Which message notifiers will be added to every subscription.'),
-      '#default_value' => \Drupal::config('message_subscribe.settings')->get('default_notifiers'),
+      '#default_value' => $config->get('default_notifiers'),
       '#multiple' => TRUE,
-      '#options' => message_notify_get_notifiers_as_options(),
+      '#options' => $options,
       '#required' => FALSE,
     ];
 
@@ -60,38 +61,37 @@ class MessageSubscribeAdminSettings extends ConfigFormBase {
       '#type' => 'checkbox',
       '#title' => t('Notify author of their own submissions'),
       '#description' => t('Determines if the user that caused the message notification receive a message about their actions. e.g. If I add a comment to a node, should I get an email saying I added a comment to a node?'),
-      '#default_value' => \Drupal::config('message_subscribe.settings')->get('notify_own_actions'),
+      '#default_value' => $config->get('notify_own_actions'),
     ];
 
-    $prefix = \Drupal::config('message_subscribe.settings')->get('flag_prefix') . '_';
+    $prefix = $config->get('flag_prefix') . '_';
 
+    // @todo
     // For every subscription flag, show a view selection.
-    foreach (message_subscribe_flag_get_flags() as $flag) {
-      $name = 'message_' . $flag->name;
-      $params = ['@title' => $flag->title];
-
-      $entity_type = FLAG_API_VERSION == 3 ? $flag->entity_type : $flag->content_type;
-
-      // @FIXME
-      // // @FIXME
-      // // The correct configuration object could not be determined. You'll need to
-      // // rewrite this call manually.
-      // $form[$name] = array(
-      //       '#type' => 'select',
-      //       '#title' => t('View for flag <em>@title</em>', $params),
-      //       '#description' => t('Select the View that should be used for flag @title.', $params),
-      //       '#options' => views_get_views_as_options(),
-      //       '#default_value' => variable_get($name, $prefix . $entity_type . ':default'),
-      //       '#required' => TRUE,
-      //     );
-
-    }
+    // foreach (message_subscribe_flag_get_flags() as $flag) {
+    //   $name = 'message_' . $flag->name;
+    //   $params = ['@title' => $flag->title];
+    //   $entity_type = FLAG_API_VERSION == 3 ? $flag->entity_type : $flag->content_type;
+    //
+    // // @FIXME
+    // // The correct configuration object could not be determined. You'll need to
+    // // rewrite this call manually.
+    // $form[$name] = array(
+    //       '#type' => 'select',
+    //       '#title' => t('View for flag <em>@title</em>', $params),
+    //       '#description' => t('Select the View that should be used for flag @title.', $params),
+    //       '#options' => views_get_views_as_options(),
+    //       '#default_value' => variable_get($name, $prefix . $entity_type . ':default'),
+    //       '#required' => TRUE,
+    //     );
+    //
+    //   }
 
     $form['flag_prefix'] = [
       '#type' => 'textfield',
       '#title' => t('Flag prefix'),
       '#description' => t('The prefix that will be used to identify subscription flags. This can be used if you already have flags defined with another prefix e.g. "follow".'),
-      '#default_value' => \Drupal::config('message_subscribe.settings')->get('flag_prefix'),
+      '#default_value' => $config->get('flag_prefix'),
       '#required' => FALSE,
     ];
 
@@ -99,7 +99,7 @@ class MessageSubscribeAdminSettings extends ConfigFormBase {
       '#type' => 'checkbox',
       '#title' => t('Use queue'),
       '#description' => t('Use the queue to process the Messages.'),
-      '#default_value' => \Drupal::config('message_subscribe.settings')->get('use_queue'),
+      '#default_value' => $config->get('use_queue'),
     ];
 
     return parent::buildForm($form, $form_state);
