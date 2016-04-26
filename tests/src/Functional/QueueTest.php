fe935af67f152fcaff53cd5cc2de238d5c6a8995
Jonathan Hedstrom <jhedstrom@gmail.com>
2016-04-26 09:00:23 -0700

    Move QueueTest to phpunit-based tests.

--- tests/src/Functional/QueueTest.php
+++ tests/src/Functional/QueueTest.php
@@ -1,18 +1,23 @@
 <?php
 
-namespace Drupal\message_subscribe\Tests;
+namespace Drupal\Tests\message_subscribe\Functional;
 
 use Drupal\message\Entity\Message;
 use Drupal\message\Entity\MessageType;
 use Drupal\message_subscribe\Exception\MessageSubscribeException;
-use Drupal\simpletest\WebTestBase;
+use Drupal\simpletest\ContentTypeCreationTrait;
+use Drupal\simpletest\NodeCreationTrait;
+use Drupal\Tests\BrowserTestBase;
 
 /**
  * Test queue integration.
  *
  * @group message_subscribe
  */
-class QueueTest extends WebTestBase {
+class QueueTest extends BrowserTestBase {
+
+  use ContentTypeCreationTrait;
+  use NodeCreationTrait;
 
   /**
    * {@inheritdoc}
@@ -55,7 +60,7 @@ class QueueTest extends WebTestBase {
     $message_type->save();
 
     // Create node-type.
-    $type = $this->drupalCreateContentType();
+    $type = $this->createContentType();
     $node_type = $type->id();
 
     // Create node.
@@ -63,7 +68,7 @@ class QueueTest extends WebTestBase {
     $settings = [];
     $settings['type'] = $node_type;
     $settings['uid'] = $user1->id();
-    $this->node = $this->drupalCreateNode($settings);
+    $this->node = $this->createNode($settings);
 
     $this->messageSubscribers = $this->container->get('message_subscribe.subscribers');
   }
@@ -85,7 +90,7 @@ class QueueTest extends WebTestBase {
       $this->fail('Can add a non-saved message to the queue.');
     }
     catch (MessageSubscribeException $e) {
-      $this->pass('Cannot add a non-saved message to the queue.');
+      $this->assertTrue(TRUE, 'Cannot add a non-saved message to the queue.');
     }
 
     // Assert message was saved and added to queue.
@@ -96,10 +101,10 @@ class QueueTest extends WebTestBase {
       'range' => 3,
     ];
     $queue = \Drupal::queue('message_subscribe');
-    $this->assertEqual($queue->numberOfItems(), 0, 'Queue is empty');
+    $this->assertEquals($queue->numberOfItems(), 0, 'Queue is empty');
     $this->messageSubscribers->sendMessage($node, $message, [], $subscribe_options);
-    $this->assertTrue($message->id(), 'Message was saved');
-    $this->assertEqual($queue->numberOfItems(), 1, 'Message added to queue.');
+    $this->assertTrue((bool) $message->id(), 'Message was saved');
+    $this->assertEquals($queue->numberOfItems(), 1, 'Message added to queue.');
 
     // Assert queue-item is processed and updated. We mock subscription
     // of users to the message. It will not be sent, as the default
@@ -118,8 +123,8 @@ class QueueTest extends WebTestBase {
 
     // Reclaim the new item, and assert the "last UID" was updated.
     $item = $queue->claimItem();
-    $this->assertNotEqual($item_id, $item->item_id, 'Queue item was updated.');
-    $this->assertEqual($item->data['subscribe_options']['last uid'], 3, 'Last processed user ID was updated.');
+    $this->assertNotEquals($item_id, $item->item_id, 'Queue item was updated.');
+    $this->assertEquals($item->data['subscribe_options']['last uid'], 3, 'Last processed user ID was updated.');
   }
 
   /**
@@ -134,18 +139,19 @@ class QueueTest extends WebTestBase {
 
     // Start with a control case.
     $this->messageSubscribers->sendMessage($node, $message, [], []);
-    $this->assertEqual($queue->numberOfItems(), 1, 'Message item 1 added to queue.');
-    $this->cronRun();
-    $this->assertEqual($queue->numberOfItems(), 0, 'Message item 1 processed by cron.');
+    $this->assertEquals($queue->numberOfItems(), 1, 'Message item 1 added to queue.');
+    $this->container->get('cron')->run();
+    $this->assertEquals($queue->numberOfItems(), 0, 'Message item 1 processed by cron.');
 
     // Now try a case where the message entity is deleted before any related
     // queue items can be processed.
     $this->messageSubscribers->sendMessage($node, $message, [], []);
-    $this->assertEqual($queue->numberOfItems(), 1, 'Message item 2 added to queue.');
+    $this->assertEquals($queue->numberOfItems(), 1, 'Message item 2 added to queue.');
     $message->delete();
     // Assert message was deleted.
-    $this->assertFalse($message->load($message->id()), 'Message entity deleted.');
-    $this->cronRun();
-    $this->assertEqual($queue->numberOfItems(), 0, 'Message item 2 processed by cron.');
+    $this->assertNull($message->load($message->id()), 'Message entity deleted.');
+    $this->container->get('cron')->run();
+    $this->assertEquals($queue->numberOfItems(), 0, 'Message item 2 processed by cron.');
   }
+
 }
