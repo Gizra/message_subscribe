796199a2fdd0173933a794e702117a19a7085f96
Jonathan Hedstrom <jhedstrom@gmail.com>
2016-10-07 13:22:16 -0700

    Use MessageTemplateCreateTrait in kernel test.

--- tests/src/Kernel/SubscribersTest.php
+++ tests/src/Kernel/SubscribersTest.php
@@ -3,18 +3,20 @@
 namespace Drupal\Tests\message_subscribe\Kernel;
 
 use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Test\AssertMailTrait;
 use Drupal\message\Entity\Message;
 use Drupal\message\Entity\MessageTemplate;
-use Drupal\simpletest\NodeCreationTrait;
 
 /**
  * Test getting subscribes from context.
  *
  * @group message_subscribe
+ *
+ * @coversDefaultClass \Drupal\message_subscribe\Subscribers
  */
 class SubscribersTest extends MessageSubscribeTestBase {
 
-  use NodeCreationTrait;
+  use AssertMailTrait;
 
   /**
    * Flag service.
@@ -112,13 +114,6 @@ class SubscribersTest extends MessageSubscribeTestBase {
     // User2 flags User1.
     $this->flagService->flag($flags['subscribe_user'], $this->users[1], $this->users[2]);
 
-    // Create a dummy message-type.
-    $message_type = MessageTemplate::create([
-      'template' => 'foo',
-      'message_text' => ['value' => 'Example text.'],
-    ]);
-    $message_type->save();
-
     // Override default notifiers.
     \Drupal::configFactory()->getEditable('message_subscribe.settings')->set('default_notifiers', [])->save();
   }
@@ -128,7 +123,7 @@ class SubscribersTest extends MessageSubscribeTestBase {
    */
   public function testGetSubscribers() {
     $message = Message::create([
-      'template' => 'foo',
+      'template' => $this->template->id(),
       'uid' => $this->users[1],
     ]);
 
@@ -153,7 +148,7 @@ class SubscribersTest extends MessageSubscribeTestBase {
 
     // Test none of users will get message if only blocked user is subscribed.
     $message = Message::create([
-      'template' => 'foo',
+      'template' => $this->template->id(),
       'uid' => $this->users[1],
     ]);
 
@@ -222,7 +217,7 @@ class SubscribersTest extends MessageSubscribeTestBase {
     // Test the affect of the variable when set to FALSE (do not notify self).
     \Drupal::configFactory()->getEditable('message_subscribe.settings')->set('notify_own_actions', FALSE)->save();
     $message = Message::create([
-      'template' => 'foo',
+      'template' => $this->template->id(),
       'uid' => $this->users[1],
     ]);
 
@@ -275,7 +270,7 @@ class SubscribersTest extends MessageSubscribeTestBase {
     // Make sure we are notifying ourselves for this test.
     \Drupal::configFactory()->getEditable('message_subscribe.settings')->set('notify_own_actions', TRUE)->save();
 
-    $message = Message::create(['template' => 'foo']);
+    $message = Message::create(['template' => $this->template->id()]);
 
     $node = $this->nodes[0];
     $node->setPublished(FALSE);
@@ -304,7 +299,7 @@ class SubscribersTest extends MessageSubscribeTestBase {
     $this->enableModules(['message_subscribe_test']);
 
     $message = Message::create([
-      'template' => 'foo',
+      'template' => $this->template->id(),
       'uid' => $this->users[1],
     ]);
 
